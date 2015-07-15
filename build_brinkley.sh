40754768e59d02be9189d1c602d2f6493c68e065
S.S. Sridhar <sarrvesh@brinkley.intra.astro.rug.nl>
2015-07-15 12:50:53 +0200

    added new build script for brinkley@kapteyn

---
+++ build_brinkley.sh
@@ -0,0 +1,20 @@
+# Rootdir of libconfig
+LIB_CONFIG_PATH="/data/users/punzo/sarrvesh/Libraries/libconfig"
+# Rootdir of cfitsio
+CFITSIO_PATH="/data/users/punzo/sarrvesh/Libraries/cfitsio"
+
+#####################################################################
+################## DO NOT EDIT BELOW THIS LINE ######################
+#####################################################################
+
+# Test if gnuplot is installed
+printf "Searching for gnuplot: "
+if ! gnuplot_loc="$(type -p gnuplot)" || [ -z "$gnuplot_loc" ]; then
+    printf "not found\n"
+else
+    printf "found\n"
+    MACRO="-DGNUPLOT_ENABLE"
+    printf "Compiling with Gnuplot\n"
+fi
+
+gcc -Wall -I${LIB_CONFIG_PATH}/include/ -I${CFITSIO_PATH}/include/ -L${LIB_CONFIG_PATH}/lib/ -L/${CFITSIO_PATH}/lib/ -o rmsynthesis src/rmsynthesis.c -lconfig -lcfitsio -lm $MACRO
